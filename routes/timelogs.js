f40127857736143582f5cd1584b5d579a38572a8
francoditopor202400712-lang <francoditopor.202400712@gmail.com>
2026-05-02 21:38:29 +0800

    logs

--- routes/timelogs.js
+++ routes/timelogs.js
@@ -54,4 +54,26 @@ router.post("/stop", (req, res) => {
   res.json(log);
 });
 
+// CREATE manual log
+router.post("/", (req, res) => {
+  const { taskId, userId, startTime, endTime, hoursWorked } = req.body;
+
+  const newLog = {
+    id: db.timelogs.length + 1,
+    taskId,
+    userId,
+    startTime: new Date(startTime),
+    endTime: new Date(endTime),
+    hoursWorked
+  };
+
+  db.timelogs.push(newLog);
+  res.status(201).json(newLog);
+});
+
+// DELETE log
+router.delete("/:id", (req, res) => {
+  db.timelogs = db.timelogs.filter(l => l.id != req.params.id);
+  res.json({ message: "Log deleted" });
+});
 module.exports = router;
